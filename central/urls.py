6b35ebf7af39a021aff6b78eb5d195177a468103
Kruze Zab <kbhattrai4@gmail.com>
2020-02-15 11:55:21 +0545

    Json retrieved

--- central/urls.py
+++ central/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('search', views.search, name='search'),
+    
+    path('lyrics', views.lyrics, name='lyrics'),
 ]
--- central/views.py
+++ central/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
+import lyricsgenius
+from pprint import pprint
 
 
 
 
 def index(request):
     return render(request, 'central/index.html')
+
+def search(request):
+    genius = lyricsgenius.Genius("4FMfq4F26i2Xz8JSEAsTInmjHBvGRK9Vz9NBT58L0ztn3bC7mPi8vg7MODxax_Pj")
+    genius.skip_non_songs = True
+
+    if 'q' in request.GET:
+        query = request.GET['q']
+        if query:
+            song = genius.search_genius(query)
+            pprint(song['hits'][1])
+
+    context = {
+        'query': query
+    }
+
+    return render(request, 'central/search.html', context)
+
+def lyrics(request):
+    return render(request, 'central/lyrics.html')
 
