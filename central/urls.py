5787886ed0113f9b1fb2095c47ee813d55504e5b
Kruze Zab <kbhattrai4@gmail.com>
2020-02-21 11:19:45 +0545

    FIxing issues

--- central/urls.py
+++ central/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('search', views.search, name='search'),
     
-    re_path('(?P<artist>.*)/(?P<song>.*)/$', views.lyrics, name='lyrics'),
+    re_path('(?P<artist>.*)-(?P<song>.*)-lyrics/$', views.lyrics, name='lyrics'),
     
 ]
 
